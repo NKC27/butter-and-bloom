57dc36546e332d9aef1d4c28a75eceb53a524e68
NKC27 <93439579+NKC27@users.noreply.github.com>
2026-08-25 20:31:25 +0100

    Fix Vercel asset paths

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// If you deploy to GitHub Pages at https://<user>.github.io/<repo>/
-// uncomment the base line below and set it to "/<repo-name>/".
 export default defineConfig({
   plugins: [react()],
-  base: '/butter-and-bloom/',
+  base: process.env.VERCEL ? '/' : '/butter-and-bloom/',
 });
